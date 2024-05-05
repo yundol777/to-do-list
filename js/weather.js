204e6867ad0d9d19b191a209106c7d344e7dba90
YunDoll <mar0722@naver.com>
2024-05-05 12:31:55 +0900

    feat: 투두 리스트 작성과 날씨 기능 추가
    사용자의 입력에 맞게 local storage를 사용해 출력 및 삭제 기능을 추가하고, api를 이용해 날씨 정보를 받아왔다.

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const API_KEY = "acd10caa49adef09327cba0c68a2d27c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherContainer = document.querySelector(
+        "#weather span:first-child"
+      );
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }); //자바스크립트가 url을 부른다.(promise)
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+// 자동으로 매개변수 하나 넘겨준다.
